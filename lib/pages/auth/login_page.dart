5d5f412cb88df8ae736e067872b349c01b15a1b8
Thiennguyen21it <thiennh.21it@vku.udn.vn>
2022-11-19 23:21:04 +0700

    change image

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -51,7 +51,7 @@ class _LoginPageState extends State<LoginPage> {
                             style: TextStyle(
                               fontSize: 15,
                             )),
-                        Image.asset("assets/Community@2x.png"),
+                        Image.asset("assets/Community.png"),
                         TextFormField(
                           decoration: textInputDecoration.copyWith(
                               labelText: "Email",
